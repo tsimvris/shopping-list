1f8fb7b57182f1f5de58246aa8b5555bd9dcdab0
Petros Tsimvris <tsimvris@gmail.com>
2022-07-29 11:20:52 +0200

    added english basic

--- src/Pages/EN.jsx
+++ src/Pages/EN.jsx
@@ -1,1 +1,60 @@
-export default function English() {}
+import React, { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+import { saveToLocalStorage, loadFromLocalStorage } from "../LocalStorage";
+import StyledInput from "../Components/StyledInput";
+
+export default function English() {
+  const [inputValue, setInputValue] = useState("");
+  const [items, setItems] = useState(loadFromLocalStorage("My Items") ?? []);
+
+  useEffect(() => {
+    saveToLocalStorage("My Items", items);
+  });
+
+  return (
+    <div className="DeWrap">
+      <h2 className="title">Shopping List</h2>
+      <ul className="ItemsUl">
+        {items.map((item) => {
+          return (
+            <>
+              <li
+                onClick={() => {
+                  setItems(items.filter((Item) => Item.id !== item.id));
+                }}
+                className="ItemsLi"
+                key={item.id}
+              >
+                {item.name}
+              </li>
+            </>
+          );
+        })}
+      </ul>
+      <h3 className="SecondaryTitle">What do you want to buy?</h3>
+
+      <form
+        className="form"
+        onSubmit={(event) => {
+          event.preventDefault();
+          setItems([...items, { name: inputValue, id: nanoid() }]);
+          setInputValue("");
+        }}
+      >
+        <StyledInput
+          required
+          type="text"
+          value={inputValue}
+          onSubmit={(event) => {
+            event.preventDefault();
+            setItems([...items, { name: inputValue, id: nanoid() }]);
+            setInputValue("");
+          }}
+          onChange={(event) => {
+            setInputValue(event.target.value);
+          }}
+        />
+      </form>
+    </div>
+  );
+}
